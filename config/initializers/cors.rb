02658a9a79110104b94bdeb6f9d331a157b81957
Shounak Rangwala <shounak.rangwala@prodigiq.com>
2025-07-09 12:04:19 -0700

    fix CORS issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4200'
-
-    resource '*',
+    origins "*"
+    resource "*",
       headers: :any,
-      methods: [:get, :post, :patch, :put, :delete, :options],
+      methods: [ :get, :post, :patch, :put, :delete, :options ],
       credentials: true
   end
 end
