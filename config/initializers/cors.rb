c2e468390b2c94da5b2f65611136ffae809e47eb
Shounak Rangwala <shounak.rangwala@prodigiq.com>
2025-07-09 12:06:36 -0700

    fix CORS issue -no wildcards

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "https://guileless-llama-116fdb.netlify.app"
     resource "*",
       headers: :any,
       methods: [ :get, :post, :patch, :put, :delete, :options ],
