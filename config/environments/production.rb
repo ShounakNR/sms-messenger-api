e43dc7080aad5eb1044ae85f93b8ae9038bf6352
Shounak Rangwala <55153887+ShounakNR@users.noreply.github.com>
2025-07-09 18:21:56 -0700

    updated the cookie_same_site_protection to avoid CORS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
-
+  config.action_dispatch.cookies_same_site_protection = :none
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
