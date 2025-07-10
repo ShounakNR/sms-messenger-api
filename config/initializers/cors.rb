0c6ccb6a1c42ec3a09fb727f40223637b93a8b52
Shounak Rangwala <55153887+ShounakNR@users.noreply.github.com>
2025-07-09 22:01:30 -0700

    updated the origin for prod

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins "https://guileless-llama-116fdb.netlify.app"
-    origins "http://localhost:4200"
+    origins "https://guileless-llama-116fdb.netlify.app"
+    # origins "http://localhost:4200"
     resource "*",
       headers: :any,
       methods: [ :get, :post, :patch, :put, :delete, :options ],
